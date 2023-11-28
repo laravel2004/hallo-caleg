7f70a9f14be90a2de2b72cce003029d190717865
daffaazhar <daffaazharsda@gmail.com>
2023-11-29 06:28:29 +0700

    make seeder for user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void {
+        $password = Hash::make('password');
+
+        // Membuat 200 data dummy dengan role 1
+        for ($i = 1; $i <= 200; $i++) {
+            DB::table('users')->insert([
+                'name' => 'User ' . $i,
+                'email' => 'user' . $i . '@example.com',
+                'password' => $password,
+                'role' => 1,
+            ]);
+        }
+    }
+}
